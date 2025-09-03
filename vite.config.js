cd1ac27576dc1329d67e28226dc7a94b99956bae
ShaikhMujahid-7080 <shaikhmujahid7080@gmail.com>
2025-09-03 20:15:44 +0530

    Index.html Undo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/academic-progress-tracker/', // <-- ADD THIS LINE
+  base: '/academic-progress-tracker/',
 });
